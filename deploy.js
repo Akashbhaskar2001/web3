e1588f801ab6b3a0dc8a6499524da49d3ac87c8e
Akash Bhaskar <90091431+Akashbhaskar2001@users.noreply.github.com>
2023-08-11 20:35:08 +0530

    Add files via upload
    My first functional smart contract on which i ran tests using mocha and deployed on sepolia test net.

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'twice address spread extra glass extend indicate dilemma despair plastic faint version',
+  // remember to change this to your own phrase!
+  'https://sepolia.infura.io/v3/8a6f5c6e63f94181a887388be985f20d'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
